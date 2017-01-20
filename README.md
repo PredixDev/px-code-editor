d45db09f39635b0d0cabe84cf0004fcbd9b09725
David Leonard <davidrobertleonard@gmail.com>
2017-01-20 11:00:27 -0800

    Add initial features for code editor component

---
+++ README.md
@@ -0,0 +1,113 @@
+# px-code-editor
+
+## Overview
+
+The px-code-editor component provides a rich, full-featured editor that can be embedded as a webcomponents and controlled through a simple attribute and event based API.
+
+Use it to provide an embedded code editor to users for editing JavaScript, HTML, CSS or other common languages. The editor is a wrapper around the open-source Ace Editor project.
+
+#### Usage
+
+Pass code in to the editor by configuring how you want it to appear then setting
+the `content` attribute (or data-binding on it) with the stringified code.
+
+Example:
+
+```
+<px-code-editor
+    language="javascript"
+    enable-beautify
+    content="var array = ['firstItem', 'secondItem'];">
+</px-code-editor>
+```
+
+#### Watching for changes
+
+The editor document will be populated by the `content` attribute, but changes to the document will not be data-bound out on the `content` attribute by default.
+
+To listen for and handle changes to the editor content, add a listener to the `px-code-editor` instance (see documentation for the `px-code-editor-content-changed` in the component demo's API section).
+
+Example:
+
+```
+<px-code-editor ...></px-code-editor>
+<script type="text/javascript">
+  var codeEditor = document.querySelector('px-code-editor');
+  codeEditor.addEventListener('px-code-editor-content-changed', function(evt) {
+    var newEditorContent = evt.detail.content;
+    // ... handle the change here ...
+  });
+</script>
+```
+
+## Usage
+
+### Prerequisites
+1. node.js
+2. npm
+3. bower
+4. [webcomponents-lite.js polyfill](https://github.com/webcomponents/webcomponentsjs)
+
+Node, npm and bower are necessary to install the component and dependencies. webcomponents.js adds support for web components and custom elements to your application.
+
+## Getting Started
+
+First, install the component via bower on the command line.
+
+```
+bower install px-code-editor --save
+```
+
+Second, import the component to your application with the following tag in your head.
+
+```
+<link rel="import" href="/bower_components/px-code-editor/px-code-editor.html"/>
+```
+
+Finally, use the component in your application:
+
+```
+<px-code-editor
+    language="javascript"
+    enable-beautify
+    content="var array = ['firstItem', 'secondItem'];">
+</px-code-editor>
+```
+
+<br />
+<hr />
+
+## Documentation
+
+Read the full API and view the demo [here](https://predixdev.github.io/px-code-editor).
+
+The documentation in this repository is supplemental to the official Predix documentation, which is continuously updated and maintained by the Predix documentation team. Go to [http://predix.io](http://predix.io)  to see the official Predix documentation.
+
+
+## Local Development
+
+From the component's directory...
+
+```
+$ npm install
+$ bower install
+$ gulp sass
+```
+
+From the component's directory, to start a local server run:
+
+```
+$ gulp serve
+```
+
+Navigate to the root of that server (e.g. http://localhost:8080/) in a browser to open the API documentation page, with link to the "Demo" / working examples.
+
+### GE Coding Style Guide
+[GE JS Developer's Guide](https://github.com/GeneralElectric/javascript)
+
+<br />
+<hr />
+
+## Known Issues
+
+Please use [Github Issues](https://github.com/PredixDev/px-code-editor/issues) to submit any bugs you might find.
---
+++ gulpfile.js
@@ -0,0 +1,132 @@
+'use strict';
+const path = require('path');
+const gulp = require('gulp');
+const pkg = require('./package.json');
+const $ = require('gulp-load-plugins')();
+const gulpSequence = require('gulp-sequence');
+const importOnce = require('node-sass-import-once');
+const stylemod = require('gulp-style-modules');
+const browserSync = require('browser-sync').create();
+const gulpif = require('gulp-if');
+const combiner = require('stream-combiner2');
+const bump = require('gulp-bump');
+const argv = require('yargs').argv;
+
+const sassOptions = {
+  importer: importOnce,
+  importOnce: {
+    index: true,
+    bower: true
+  }
+};
+
+gulp.task('clean', function() {
+  return gulp.src(['.tmp', 'css'], {
+    read: false
+  }).pipe($.clean());
+});
+
+function handleError(err){
+  console.log(err.toString());
+  this.emit('end');
+}
+
+function buildCSS(){
+  return combiner.obj([
+    $.sass(sassOptions),
+    $.autoprefixer({
+      browsers: ['last 2 versions', 'Safari 8.0'],
+      cascade: false
+    }),
+    gulpif(!argv.debug, $.cssmin())
+  ]).on('error', handleError);
+}
+
+gulp.task('sass', function() {
+  return gulp.src(['./sass/*.scss', '!./sass/*sketch.scss'])
+    .pipe(buildCSS())
+    .pipe(gulpif(/.*predix/,
+      $.rename(function(path){
+        path.basename = new RegExp('.+?(?=\-predix)').exec(path.basename)[0];
+      })
+    ))
+    .pipe(stylemod({
+      moduleId: function(file) {
+        return path.basename(file.path, path.extname(file.path)) + '-styles';
+      }
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream({match: 'css/*.html'}));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['sass/*.scss'], ['sass']);
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    port: 8080,
+    notify: false,
+    reloadOnRestart: true,
+    logPrefix: `${pkg.name}`,
+    https: false,
+    server: ['./', 'bower_components'],
+  });
+
+  gulp.watch(['css/*-styles.html', '*.html', '*.js']).on('change', browserSync.reload);
+  gulp.watch(['sass/*.scss'], ['sass']);
+});
+
+gulp.task('bump:patch', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:minor', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:major', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'major'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', function(callback) {
+  gulpSequence('clean', 'sass')(callback);
+});
+
+/**
+ * Custom for this component: Build the ace theme from ace-theme/*.
+ */
+
+const replace = require('gulp-replace');
+const fs = require('fs');
+
+gulp.task('editor', function(callback) {
+  gulpSequence('editor:sass', 'editor:js', 'editor:clean')(callback);
+});
+
+gulp.task('editor:sass', function() {
+  return gulp.src(['./ace-theme/*.scss'])
+    .pipe(buildCSS())
+    .pipe(gulp.dest('ace-theme'))
+});
+
+gulp.task('editor:js', function(){
+  gulp.src(['./ace-theme/theme-predix.js'])
+    .pipe(replace(/\/\*GULP_WILL_ADD_INLINE_CSS_HERE\*\//g, '"' + readThemeCSS() + '"'))
+    .pipe(gulp.dest('ace'));
+});
+
+gulp.task('editor:clean', function(){
+  gulp.src(['./ace-theme/*.css'])
+    .pipe($.clean());
+});
+
+function readThemeCSS() {
+  return fs.readFileSync('ace-theme/theme-predix.css', 'utf8');
+}
